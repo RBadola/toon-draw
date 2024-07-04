0183ab5708f3fb9754231f839c12525e0e572625
Roshan Kumar Badola <roshanbadola06@gmail.com>
2024-07-04 16:18:22 +0530

    code update

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
-var r = document.querySelector(':root');
-const iconNames =[
-    "default",
-    "brush",
-    "pen",
-    "bin",
-    "colors",
-    "bucket",
-    "download",
-    "eraser",
-    "redo",
-    "undo",
-    "share"
-]
+var r = document.querySelector(":root");
+const iconNames = ["default", "brush", "pen", "bucket", "eraser"];
 // change cursor according to button
-function changeCursor(event){
-const icon =event.target.dataset.name
-if( iconNames.includes(icon) ){
-console.log(true);
-myFunction_set(icon)
-document.body.style.cursor = `url(./assets/${icon}.png), auto`;
-}
-}
-
-//  set cursor image
-function myFunction_set(icon) {
-r.style.setProperty('--cursoricon',`url(./assets/${icon}.png), auto`);
+function changeCursor(event) {
+  const icon = event.target.dataset.name;
+  if(icon === "default" || !iconNames.includes(icon)) {
+    document.body.style.cursor = `url(), auto`;
+    r.style.setProperty("--cursoricon", `url(), auto`);
+    return
+  }
+  if(iconNames.includes(icon)) {
+   
+      document.body.style.cursor = `url(./assets/${icon}.png), auto`;
+      r.style.setProperty("--cursoricon", `url(./assets/${icon}.png), auto`);
+  }
 }
 
 // do functions that are onclick only
-function utility(event){
-    console.log(event);
+function utility(event) {
+  console.log(event);
 }
