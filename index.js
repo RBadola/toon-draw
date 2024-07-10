01f19d7eb135beb0fbc15a5f069e1530b35c7b80
Roshan Kumar Badola <roshanbadola06@gmail.com>
2024-07-10 18:08:35 +0530

    Adding functionality

--- index.js
+++ index.js
@@ -19,3 +19,43 @@ function changeCursor(event) {
 function utility(event) {
   console.log(event);
 }
+
+//  canvas functionality
+ const canvas = document.querySelector('canvas')
+ const ctx = canvas.getContext("2d")
+//   rectangle filled with color
+// ctx.fillRect(10,10,100,100)
+// change style
+// ctx.lineWidth = 1
+// ctx.strokeStyle = "red";
+// // rectangle with outline
+// ctx.strokeRect(10,10,100,100)
+
+// drawing line
+
+//  initiate the line
+// ctx.beginPath()
+// //  set sgtarting point
+// ctx.moveTo(10,10)
+// // set line direction
+// ctx.lineTo(100,10) 
+// ctx.lineTo(100,150)
+// // close the line
+// ctx.closePath()
+// // make line visible
+// ctx.stroke()
+
+
+
+
+
+
+
+// download function
+
+const downloadDrawing=()=>{
+  let link = document.querySelector("#downloadlink")
+  link.href = canvas.toDataURL()
+  link.click()
+}
+document.getElementById("downloadButton").addEventListener('click',downloadDrawing)
